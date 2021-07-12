88b02bd6ae8d07b37471089781e65570dbd45e61
rightvalue <bluebird6922@gmail.com>
2021-07-12 16:44:44 +0300

    AddDeploymentKey

--- go.mod
+++ go.mod
@@ -6,9 +6,10 @@ require (
 	github.com/canthefason/go-watcher v0.2.4 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fatih/color v1.12.0 // indirect
-	github.com/gin-gonic/gin v1.6.3
+	github.com/gin-gonic/gin v1.7.2
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/joho/godotenv v1.3.0
 	github.com/myesui/uuid v1.0.0 // indirect
 	github.com/twinj/uuid v1.0.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 )
--- handler/handler.go
+++ handler/handler.go
@@ -2,16 +2,23 @@ package handlers
 
 import (
 	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"jwt-app/auth"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/ssh"
 )
 
 // ProfileHandler struct
@@ -339,6 +346,66 @@ func (h *profileHandler) AddSSHKey(c *gin.Context) {
 	})
 }
 
+func generatePrivateKey(bitSize int) (*rsa.PrivateKey, error) {
+	// Private Key generation
+	privateKey, err := rsa.GenerateKey(rand.Reader, bitSize)
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate Private Key
+	err = privateKey.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Private Key generated")
+	return privateKey, nil
+}
+
+// encodePrivateKeyToPEM encodes Private Key from RSA to PEM format
+func encodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+	// Get ASN.1 DER format
+	privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+
+	// pem.Block
+	privBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privDER,
+	}
+
+	// Private key in PEM format
+	privatePEM := pem.EncodeToMemory(&privBlock)
+
+	return privatePEM
+}
+
+// generatePublicKey take a rsa.PublicKey and return bytes suitable for writing to .pub file
+// returns in the format "ssh-rsa ..."
+func generatePublicKey(privatekey *rsa.PublicKey) ([]byte, error) {
+	publicRsaKey, err := ssh.NewPublicKey(privatekey)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKeyBytes := ssh.MarshalAuthorizedKey(publicRsaKey)
+
+	log.Println("Public key generated")
+	return pubKeyBytes, nil
+}
+
+// writePemToFile writes keys to a file
+func writeKeyToFile(keyBytes []byte, saveFileTo string) error {
+	err := ioutil.WriteFile(saveFileTo, keyBytes, 0600)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Key saved to: %s", saveFileTo)
+	return nil
+}
+
 func (h *profileHandler) AddDeploymentKey(c *gin.Context) {
 	mapToken := map[string]string{}
 	if err := c.ShouldBindJSON(&mapToken); err != nil {
@@ -349,9 +416,38 @@ func (h *profileHandler) AddDeploymentKey(c *gin.Context) {
 	username := mapToken["name"]
 	password := mapToken["password"]
 	log.Println(fmt.Sprintf("Add SSH key, username: %s, password: %s", username, password))
+	////////////////////////////////////////////////////
+	savePrivateFileTo := "./id_rsa_test"
+	savePublicFileTo := "./id_rsa_test.pub"
+	bitSize := 4096
 
-	c.JSON(http.StatusCreated, map[string]bool{
-		"success": true,
+	privateKey, err := generatePrivateKey(bitSize)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	publicKeyBytes, err := generatePublicKey(&privateKey.PublicKey)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	privateKeyBytes := encodePrivateKeyToPEM(privateKey)
+
+	err = writeKeyToFile(privateKeyBytes, savePrivateFileTo)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	err = writeKeyToFile([]byte(publicKeyBytes), savePublicFileTo)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	myString := string(publicKeyBytes[:])
+	////////////////////////////////////////////////////
+	c.JSON(http.StatusCreated, map[string]string{
+		"success": strconv.FormatBool(true),
+		"pub_key": myString,
 	})
 }
 
